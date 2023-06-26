5bd829511b83f94523403db75ea093b791b08aeb
Joren Vos <joren.vos@gmail.com>
2023-06-26 15:19:41 +0200

    Fix updating env variable

--- step.sh
+++ step.sh
@@ -2,4 +2,7 @@
 set -ex
 
 OUTPUT=$( git log --merges --date=format-local:'%d-%m-%Y @ %H:%M' --format='- %s - %cd' "-n${number_of_commits}" )
-envman add --key BITRISE_MERGES_LOG --value ${OUTPUT}
+
+envman add --key BITRISE_MERGES_LOG --value "${OUTPUT}"
+
+echo "${BITRISE_MERGES_LOG}"
