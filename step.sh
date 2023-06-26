d4da058e7b009ad7dc65aaa134e0d77c93b63ab7
Joren Vos <joren.vos@gmail.com>
2023-06-26 15:35:06 +0200

    Remove echo

--- step.sh
+++ step.sh
@@ -4,5 +4,3 @@ set -ex
 OUTPUT=$( git log --merges --date=format-local:'%d-%m-%Y @ %H:%M' --format='- %s - %cd' "-n${number_of_commits}" )
 
 envman add --key BITRISE_MERGES_LOG --value "${OUTPUT}"
-
-echo "${BITRISE_MERGES_LOG}"
