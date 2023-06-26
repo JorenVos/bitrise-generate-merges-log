d22fe86280b7d7c9ddeb89c224324ccecbdf3aaa
Joren Vos <joren.vos@gmail.com>
2023-06-26 14:12:31 +0200

    Add debug logging + remove return

--- step.sh
+++ step.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -ex
 
+print pwd
+
 echo "This is the value specified for the input 'number_of_commits': ${number_of_commits}"
 
-output=$(git log --merges --date=format-local:'%d-%m-%Y @ %H:%M' --format='- %s - %cd' "-n${number_of_commits}")
+git log --merges --date=format-local:'%d-%m-%Y @ %H:%M' --format='- %s - %cd' "-n${number_of_commits}"
 
-echo "Output = ${output}"
+OUTPUT=$( git log --merges --date=format-local:'%d-%m-%Y @ %H:%M' --format='- %s - %cd' "-n${number_of_commits}" )
+echo "Output = ${OUTPUT}"
 
-envman add --key BITRISE_MERGES_LOG --value output
-
-return 0
+envman add --key BITRISE_MERGES_LOG --value OUTPUT
