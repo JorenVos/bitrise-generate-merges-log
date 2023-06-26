72590be62545c6d3fc83e5f800fa1826caa4b2ae
Joren Vos <joren.vos@gmail.com>
2023-06-26 15:06:09 +0200

    Fix output env var

--- step.sh
+++ step.sh
@@ -2,4 +2,4 @@
 set -ex
 
 OUTPUT=$( git log --merges --date=format-local:'%d-%m-%Y @ %H:%M' --format='- %s - %cd' "-n${number_of_commits}" )
-envman add --key BITRISE_MERGES_LOG --value OUTPUT
+envman add --key BITRISE_MERGES_LOG --value ${OUTPUT}
