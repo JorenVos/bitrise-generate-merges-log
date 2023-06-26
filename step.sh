fbb635724648e3acc0f84c23ef9d7af5efc4f72f
Joren Vos <joren.vos@gmail.com>
2023-06-26 14:30:36 +0200

    Remove debug code

--- step.sh
+++ step.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 set -ex
 
-pwd
-
-echo "This is the value specified for the input 'number_of_commits': ${number_of_commits}"
-
-git log --merges --date=format-local:'%d-%m-%Y @ %H:%M' --format='- %s - %cd' "-n${number_of_commits}"
-
 OUTPUT=$( git log --merges --date=format-local:'%d-%m-%Y @ %H:%M' --format='- %s - %cd' "-n${number_of_commits}" )
-echo "Output = ${OUTPUT}"
-
 envman add --key BITRISE_MERGES_LOG --value OUTPUT
